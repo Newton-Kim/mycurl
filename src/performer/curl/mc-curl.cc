fa4035edf2643c851b4a646d270bf2484be0efb1
Newton <Newton>
2015-06-20 12:35:02 +0900

    Redirecting the response data to a file.

--- src/performer/curl/mc-curl.cc
+++ src/performer/curl/mc-curl.cc
@@ -1,10 +1,22 @@
 #include "mc-curl.h"
 #include <stdexcept>
+#include <cstring>
+#include <cerrno>
+
+size_t mcCurlwriteCallback(char *ptr, size_t size, size_t nmemb, void *userdata){
+	FILE* fd = (FILE*)userdata;
+	if(fd) fd = stdout;
+	string str;
+	str.append(ptr, size * nmemb);
+	fprintf(fd, "%s", str.c_str());
+	return size * nmemb;
+}
 
 mcCurl::mcCurl(string url, string mnymonic):m_url(url), m_mnymonic(mnymonic), m_verbose(false){
 	m_curl = curl_easy_init();
 	if(!m_curl) throw runtime_error("curl_easy_init failed");
 	curl_easy_setopt(m_curl, CURLOPT_URL, m_url.c_str());
+	curl_easy_setopt(m_curl, CURLOPT_WRITEFUNCTION, mcCurlwriteCallback);
 }
 
 mcCurl::~mcCurl(){
@@ -39,7 +51,14 @@ void mcCurl::get(string path, string lst){
 	curl_easy_setopt(m_curl, CURLOPT_POST, 0);
 	curl_easy_setopt(m_curl, CURLOPT_PUT, 0);
 	curl_easy_setopt(m_curl, CURLOPT_CUSTOMREQUEST, NULL);
+	FILE* fd = NULL;
+	if(path.size()) {
+		fd = fopen(path.c_str(), "w");
+		if(!fd) throw runtime_error(strerror(errno));
+		curl_easy_setopt(m_curl, CURLOPT_WRITEDATA, fd);
+	}
 	curl_easy_perform(m_curl);
+	if(fd) fclose(fd);
 }
 
 void mcCurl::del(string lst){
@@ -74,7 +93,7 @@ void mcCurl::put(string inpath, size_t chunk, string outpath, string lst){
 
 void mcCurl::header(string key, string value, string lst){
 	map<string, curl_slist*>::iterator it = m_headers.find(lst);
-	string hdr = key + ":" + value;
+	string hdr = key + ": " + value;
 	curl_slist* slist = (it != m_headers.end()) ? it->second : NULL;
 	slist = curl_slist_append(slist, hdr.c_str());
 	m_headers[lst] = slist;
