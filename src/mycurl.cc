d74350da3fc794f5621a283e07f796ce1f3ca098
Newton <Newton>
2015-06-27 12:42:18 +0900

    Debian packaging has been added.

--- src/mycurl.cc
+++ src/mycurl.cc
@@ -2,10 +2,32 @@
 #include "performer/mc-curl-performer.h"
 #include "version.h"
 #include <cstdio>
+#include <ctime>
+#include <cstring>
 
 #define LINE_SIZE 1024
 void show_banner(void) {
-  fprintf(stdout, "mycurl version %s by Newton Kim\n", VERSION);
+  fprintf(stdout, "mycurl %s\n", VERSION);
+  {
+    time_t rawtime;
+    time (&rawtime);
+    struct tm * timeinfo = localtime(&rawtime);
+    fprintf(stdout, "Copyright(C) %d Newton Kim (newton.s.kim@gmail.com)\n", timeinfo->tm_year + 1900);
+    fprintf(stdout, "License MIT: MIT License(http://opensource.org/licenses/MIT)\n");
+  }
+  fprintf(stdout, "\n");
+}
+
+void show_version(void) {
+    fprintf(stdout, "%s\n", VERSION);
+}
+
+void show_help(void) {
+  fprintf(stdout, "Usage: mycurl [option]... [script]...\n");
+  fprintf(stdout, "\n");
+  fprintf(stdout, "Option:\n");
+  fprintf(stdout, "  -h, --help     shows this help screen.\n");
+  fprintf(stdout, "  -v, --version  shows the version.\n");
 }
 
 int main(int argc, char* argv[]) {
@@ -14,7 +36,14 @@ int main(int argc, char* argv[]) {
   mcLanguage language(&performer);
   mcLanguageState state = MC_LANG_CONTINUE;
   for (int i = 1; i < argc && state == MC_LANG_CONTINUE; i++) {
-    if (argv[i][0] == '-') {
+    if (!strcmp(argv[i], "--help") || !strcmp(argv[i], "-h")) {
+      show_banner();
+      show_help();
+      return 0;
+    } else if (!strcmp(argv[i], "--version") || !strcmp(argv[i], "-v")) {
+      show_version();
+      return 0;
+    } else if (argv[i][0] == '-') {
       fprintf(stderr, "invalid argument %s\n", argv[i]);
       return 1;
     }
