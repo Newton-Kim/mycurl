7a07a39a72ed042f32eab194c8f58b6e21945524
Newton <newton.s.kim@gmail.com>
2015-07-07 01:19:47 +0900

    null header is enabled

--- src/interpreter/command/mc-cmd-header.cc
+++ src/interpreter/command/mc-cmd-header.cc
@@ -2,7 +2,7 @@
 #include <cstdio>
 
 void mcCmdHeader::help(void) {
-  fprintf(stdout, "  Usage: header key value [> list]\n");
+  fprintf(stdout, "  Usage: header key [value] [> list]\n");
   fprintf(stdout, "  Option:\n");
   fprintf(stdout, "    sets a header to a list.\n");
   fprintf(stdout, "    Default list is defhdr.\n");
@@ -20,8 +20,9 @@ mcLanguageState mcCmdHeader::parse(mcScanner& scanner,
   }
   token = scanner.tokenize();
   if (token.id != MC_TOKEN_STRING) {
-    fprintf(stderr, "value is missing\n");
-    return MC_LANG_CONTINUE;
+//    fprintf(stderr, "value is missing\n");
+//    return MC_LANG_CONTINUE;
+    val.clear();
   } else {
     val = token.buffer;
   }
