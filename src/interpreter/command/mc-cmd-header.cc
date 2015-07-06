16e6d3b65d13a601afadad1e77af1bcffd1eea58
Newton <newton.s.kim@gmail.com>
2015-07-07 02:22:53 +0900

    null header was revoked

--- src/interpreter/command/mc-cmd-header.cc
+++ src/interpreter/command/mc-cmd-header.cc
@@ -2,7 +2,7 @@
 #include <cstdio>
 
 void mcCmdHeader::help(void) {
-  fprintf(stdout, "  Usage: header key [value] [> list]\n");
+  fprintf(stdout, "  Usage: header key value [> list]\n");
   fprintf(stdout, "  Option:\n");
   fprintf(stdout, "    sets a header to a list.\n");
   fprintf(stdout, "    Default list is defhdr.\n");
@@ -20,9 +20,8 @@ mcLanguageState mcCmdHeader::parse(mcScanner& scanner,
   }
   token = scanner.tokenize();
   if (token.id != MC_TOKEN_STRING) {
-//    fprintf(stderr, "value is missing\n");
-//    return MC_LANG_CONTINUE;
-    val.clear();
+    fprintf(stderr, "value is missing\n");
+    return MC_LANG_CONTINUE;
   } else {
     val = token.buffer;
   }
